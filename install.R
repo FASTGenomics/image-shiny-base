3e17f5b4d7e5de95c129ad4cc5ce78ed97aab1b2
Benedikt <benedikt.reiz@comma-soft.com>
2021-08-30 13:18:41 +0200

    :zap: Improve error handling

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 # Use this file to install your requirements
-
+options(warn = 2) # stop installation  at warning // without this failing package installations will most likely be overseen
 ##############################
 ### Examples on how to use: ##
 #
